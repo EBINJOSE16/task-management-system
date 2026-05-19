aadecda7ab9839b4d98eaa243c74536cf8d010b5
EBIN JOSE <ebinj094@gmail.com>
2026-05-19 11:30:27 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+//{app.use(cors({
+  //origin: "https://your-frontend.vercel.app",
+  //credentials: true,
+//}));}
 app.use(express.json());
 
 app.use("/api", authRoutes);
