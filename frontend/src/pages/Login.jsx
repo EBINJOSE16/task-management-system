5bfbeb440f5ab5072ea37ab5855360120e75c93f
EBIN JOSE <ebinj094@gmail.com>
2026-05-19 14:28:53 +0530

    fix: react routil

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -57,11 +57,7 @@ const Login = () => {
     }
     const user = await User.findOne({ email });
 
-if (!user) {
-  return res.status(404).json({
-    message: "User not found",
-  });
-}
+
 
     return newErrors;
   };
