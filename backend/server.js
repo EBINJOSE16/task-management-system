583a1043fbeedc05c86deb817fcb2cda73708007
EBIN JOSE <ebinj094@gmail.com>
2026-05-19 17:09:24 +0530

    fix render crash

--- backend/server.js
+++ backend/server.js
@@ -13,15 +13,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "https://task-management-system-delta-three.vercel.app",
-    "https://task-management-system-git-master-ebinj094-1772s-projects.vercel.app"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "https://task-management-system-delta-three.vercel.app",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
